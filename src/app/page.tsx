54b21263f08d54f22dfbc620ebb64a2f3b576f0c
rofireza <arrofirezasatria@gmail.com>
2023-11-22 07:22:10 +0700

    update container dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,10 @@ import Image from "next/image";
 import AppsContainer from "@/components/AppsContainer";
 import AppsNavbar from "@/components/AppsNavbar";
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AppsNavbar />
-      <AppsContainer>
-        <div>this is home page</div>
-      </AppsContainer>
+      <>{children}</>
     </main>
   );
 }
-  
--- src/components/AppsNavbar.tsx
+++ src/components/AppsNavbar.tsx
@@ -25,21 +25,6 @@ export default function AppsNavbar() {
           aria-expanded="false"
         >
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
