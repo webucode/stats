770ac3c3f3390ec052b4cfbcae0842c3d63a36cc
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-12-22 19:47:51 +0700

    update

--- src/app/dashboard/showcase/components/FormShowcase.tsx
+++ src/app/dashboard/showcase/components/FormShowcase.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import React from "react";
-import { useFormState, useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type Inputs = {
-  deviceName: string;
-  description: string;
-};
+import * as z from "zod";
+
+const formSchema = z.object({
+  deviceName: z.string().min(2, { message: "ada a das dasda" }).max(10, {
+    message: "Username must be at least 2 characters.",
+  }),
+  description: z.string(),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
 
 export default function FormShowcase({ action }: { action: any }) {
   const [state, formAction] = useFormState(action, null);
@@ -18,7 +26,9 @@ export default function FormShowcase({ action }: { action: any }) {
     watch,
     reset,
     formState: { isSubmitting, errors, isValidating },
-  } = useForm<Inputs>();
+  } = useForm<TFormSchema>({
+    resolver: zodResolver(formSchema),
+  });
 
   React.useEffect(() => {
     state === "Created" && reset();
@@ -27,8 +37,13 @@ export default function FormShowcase({ action }: { action: any }) {
   return (
     <div>
       <form action={formAction} onSubmit={() => console.log("on submit")}>
-        <input {...register("deviceName", { required: true })} />
-        <input {...register("description", { required: true })} />
+        <Input {...register("deviceName", { required: true })} />
+        {errors.deviceName && (
+          <p className="text-red-500">{`${errors.deviceName.message}`}</p>
+        )}
+        <Input {...register("description", { required: true })} />
+        {/* <input {...register("deviceName", { required: true })} /> */}
+        {/* <input {...register("description", { required: true })} /> */}
         <button type="submit">{isValidating ? "...Loading" : "Submit"}</button>;
       </form>
     </div>
